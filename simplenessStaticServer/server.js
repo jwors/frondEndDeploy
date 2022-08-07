43914327797a862ecc11e91080bc800e72996488
我可真猛啊 <j.wors@outlook.com>
2022-08-07 19:15:09 +0800

    提交

--- simplenessStaticServer/server.js
+++ simplenessStaticServer/server.js
@@ -1,10 +1,10 @@
 const http = require('node:http')
 const fs = require('node:fs')
 
-const html = fs.readFileSync('./hello.html')
+const html = fs.readFileSync('./index.html')
 
 const server = http.createServer((req, res) => {
-    fs.createReadStream('./hello.html').pipe(res)
+    fs.createReadStream('./index.html').pipe(res)
 })
 server.listen(3000, () => {
     console.log('Listening 3000');
